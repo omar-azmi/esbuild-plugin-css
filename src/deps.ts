31730578213cd0902019bebd0175fda380f2f0b2
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-03-25 15:17:45 -0400

    - remove dependency on Deno for resolving paths. (but as a consequence we won't be able to resolve `jsr:` and `npm:` namespace imports) - bundle css that gets imported by another css file via `@import(namespace://path/to/file.css)`, when using the "inject" plugin option. it works by bundling the to-be-injected css file on its own, and then converting the output css text into a javascript style injection code. - the resolver now captures all namespaces, because the deno plugin will sometimes modify the css import namespace to `"http"`, or `"data"`, etc.. , so that it no longer exclusively remains only `"file"` and/or `plugin_namespace` (`"css-ns"`).

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
 export type * as esbuild from "https://deno.land/x/esbuild@v0.20.1/mod.js"
-export { dirname as pathDirname, join as pathJoin } from "jsr:@std/path@0.218.2"
-
+export { dirname as pathDirname, join as pathJoin, toFileUrl } from "jsr:@std/path@0.218.2"
+import type * as esbuild from "https://deno.land/x/esbuild@v0.20.1/mod.js"
 
 /** concatenate a bunch of `Uint8Array` and `Array<number>` into a single `Uint8Array` array */
 export const concatBytes = (...arrs: (Uint8Array | Array<number>)[]): Uint8Array => {
@@ -10,3 +10,42 @@ export const concatBytes = (...arrs: (Uint8Array | Array<number>)[]): Uint8Array
 	for (const arr of arrs) outarr.set(arr, offsets.shift())
 	return outarr
 }
+
+/** recognized namespaces.
+ * - `absolute-file`: "C://absolute/path/to/file.txt"
+ * - `relative-file`: "./path/to/file.txt"
+ * - `file`: "file://C://absolute/path/to/file.txt"
+ * - `http`: "http://example.com/path/to/file.txt"
+ * - `https`: "https://example.com/path/to/file.txt"
+ * - `data`: "data:text/plain;base64,SGVsbG9Xb3JsZA==" or "data:text/plain,HelloWorld"
+ * - `jsr`: "jsr:@scope/package-name"
+ * - `npm`: "npm:@scope/package-name" or "npm:package-name"
+*/
+type Namespace =
+	| undefined
+	| "absolute-file"
+	| "relative"
+	| "file"
+	| "http"
+	| "https"
+	| "data"
+	| "jsr"
+	| "npm"
+
+export const getUriNamespace = (path: string): Namespace => {
+	if (!path || path === "") { return undefined }
+	if (path.startsWith("npm:")) { return "npm" }
+	if (path.startsWith("jsr:")) { return "jsr" }
+	if (path.startsWith("data:")) { return "data" }
+	if (path.startsWith("http://")) { return "http" }
+	if (path.startsWith("https://")) { return "https" }
+	if (path.startsWith("file://")) { return "file" }
+	if (path.startsWith("./") || path.startsWith("../")) { return "relative" }
+	return "absolute-file"
+}
+
+// type UriArgs = Pick<esbuild.OnResolveArgs, "path" | "importer" | "resolveDir"> & { namespace?: Namespace }
+
+// export const uriToUrl = (args: UriArgs): URL => {
+// 	new URL(args.path)
+// }
--- src/mod.ts
+++ src/mod.ts
@@ -1,79 +1,73 @@
-import { esbuild, pathJoin } from "./deps.ts"
+import { esbuild, getUriNamespace } from "./deps.ts"
 
-// const css_importer_map = new Map<string, string>()
+const css_importer_map = new Map<string, string>()
 
-interface PluginOptions {
+export interface PluginOptions {
 	mode?: "bundle" | "inject" | "inject-link"
 }
+
+interface CssPluginData {
+	kind: esbuild.OnResolveArgs["kind"]
+}
+
+const plugin_namespace = "css-ns" as const
 
 export const denoCssPlugin = (options: PluginOptions = {}): esbuild.Plugin => {
 	return {
 		name: "deno-css-plugin",
 		setup: (build) => {
-			build.onResolve({ filter: /\.css$/ }, (args) => {
+			const pathResolver = (args: esbuild.OnResolveArgs): esbuild.OnResolveResult => {
 				console.log(args)
-				const { resolveDir, path, namespace } = args
-				return { path: pathJoin(resolveDir, path), namespace }
-			})
-
-			const transform_loaded_css = async (css_content: string): Promise<esbuild.OnLoadResult> => {
-				const { minify, minifySyntax, minifyWhitespace } = build.initialOptions
-				const apply_minification = async (css_text: string) => {
-					if (!minify && !minifySyntax && !minifyWhitespace) {
-						// no minification was requested
-						return css_text
-					}
-					return (await build.esbuild.transform(css_text, {
-						loader: "css",
-						minify,
-						minifySyntax,
-						minifyWhitespace,
-					})).code
+				const { path, importer, kind } = args
+				const
+					importer_namespace = getUriNamespace(importer),
+					importer_url = kind === "entry-point"
+						? undefined
+						: importer_namespace === "absolute-file"
+							? new URL("file://" + importer)
+							: new URL(importer),
+					path_namespace = getUriNamespace(path),
+					path_url = path_namespace === "absolute-file"
+						? new URL("file://" + path)
+						: path_namespace === "relative"
+							? new URL(path, importer_url)
+							: new URL(path)
+				console.log("css path resolved to:", path_url.toString())
+				return {
+					path: path_url.toString(),
+					namespace: plugin_namespace,
+					pluginData: { kind }
 				}
+			}
 
-				if (options.mode === "inject") {
-					css_content = await apply_minification(css_content)
+			build.onResolve({ filter: /\.css$/ }, pathResolver)
+			// esbuild labels everything as a "file" in the very beginning. so it is up to us to correct it.
+			// build.onResolve({ filter: /\.css$/, namespace: "file" }, pathResolver)
+			// build.onResolve({ filter: /\.css$/, namespace: plugin_namespace }, pathResolver)
+			build.onLoad({ filter: /.*/, namespace: plugin_namespace }, async (args) => {
+				console.log(args)
+				const { kind: importer_kind } = args.pluginData as CssPluginData
+				if (options.mode === "inject" && importer_kind === "import-statement" || importer_kind === "dynamic-import") {
+					// if we are to inject the css, then we'll need to bundle the css file of interest,
+					// using a new `esbuild.build` process, with this very same plugin applied.
+					// we cannot use `esbuild.transform` because it is possible for the css file of interest to import other css files as well.
+					// which is something that the transformer will ignore, rather than resolving it.
+					const css_content = (await build.esbuild.build({
+						...build.initialOptions,
+						entryPoints: [args.path],
+						plugins: [denoCssPlugin()],
+						bundle: true,
+						write: false,
+					})).outputFiles.pop()!.text
 					const js_text = `
 const style_dom = document.createElement("style")
 style_dom.textContent = \`${css_content}\`
 document.head.append(style_dom)`
 					return { loader: "js", contents: js_text }
-				} else if (options.mode === "inject-link") {
-					css_content = await apply_minification(css_content)
-					const js_text = `
-const css_link_dom = document.createElement("link")
-css_link_dom.rel = "stylesheet"
-css_link_dom.href = "PATH_TO_COMPILED_CSS"
-document.head.append(css_link_dom)`
-					// TODO: find a way to figure out the path of the compiled css before hand
-					// return {
-					// 	loader: "js",
-					// 	contents: "",
-					// 	then: (value: esbuild.OnLoadResult) => {
-					// 		value
-					// 	}
-					// }
 				}
+				const css_content = await (await fetch(new URL(args.path))).text()
 				return { loader: "css", contents: css_content }
-			}
-
-			build.onLoad({ filter: /\.css$/, namespace: "file" }, async (args: esbuild.OnLoadArgs) => {
-				const contents = await Deno.readTextFile(args.path)
-				// TODO: apply transformation only if resolver's `args.kind === "import-statement"`.
-				// unfortunately, the `kind` member isn't passed onto the loader's `args`. so I cannot check directly
-				return transform_loaded_css(contents)
 			})
-
-			const http_loader = async (args: esbuild.OnLoadArgs): Promise<esbuild.OnLoadResult> => {
-				const contents = await (await fetch(`${args.namespace}:${args.path}`)).text()
-				// TODO: apply transformation only if resolver's `args.kind === "import-statement"`.
-				// unfortunately, the `kind` member isn't passed onto the loader's `args`. so I cannot check directly
-				return transform_loaded_css(contents)
-			}
-
-			build.onLoad({ filter: /\.css$/, namespace: "data" }, http_loader)
-			build.onLoad({ filter: /\.css$/, namespace: "http" }, http_loader)
-			build.onLoad({ filter: /\.css$/, namespace: "https" }, http_loader)
 		}
 	}
 }
