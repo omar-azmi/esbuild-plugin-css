864cb3eb1b58a5715fabd414b8842f84e08732ce
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-03-25 15:10:06 -0400

    add options to `denoCssPlugin` for injecting the css via javascript using an HTMLStyleElement.

--- src/mod.ts
+++ src/mod.ts
@@ -1,8 +1,12 @@
-import { concatBytes, esbuild, pathDirname, pathJoin } from "./deps.ts"
+import { esbuild, pathJoin } from "./deps.ts"
 
-const css_importer_map = new Map<string, string>()
+// const css_importer_map = new Map<string, string>()
 
-export const denoCssPlugin = (): esbuild.Plugin => {
+interface PluginOptions {
+	mode?: "bundle" | "inject" | "inject-link"
+}
+
+export const denoCssPlugin = (options: PluginOptions = {}): esbuild.Plugin => {
 	return {
 		name: "deno-css-plugin",
 		setup: (build) => {
@@ -12,14 +16,59 @@ export const denoCssPlugin = (): esbuild.Plugin => {
 				return { path: pathJoin(resolveDir, path), namespace }
 			})
 
+			const transform_loaded_css = async (css_content: string): Promise<esbuild.OnLoadResult> => {
+				const { minify, minifySyntax, minifyWhitespace } = build.initialOptions
+				const apply_minification = async (css_text: string) => {
+					if (!minify && !minifySyntax && !minifyWhitespace) {
+						// no minification was requested
+						return css_text
+					}
+					return (await build.esbuild.transform(css_text, {
+						loader: "css",
+						minify,
+						minifySyntax,
+						minifyWhitespace,
+					})).code
+				}
+
+				if (options.mode === "inject") {
+					css_content = await apply_minification(css_content)
+					const js_text = `
+const style_dom = document.createElement("style")
+style_dom.textContent = \`${css_content}\`
+document.head.append(style_dom)`
+					return { loader: "js", contents: js_text }
+				} else if (options.mode === "inject-link") {
+					css_content = await apply_minification(css_content)
+					const js_text = `
+const css_link_dom = document.createElement("link")
+css_link_dom.rel = "stylesheet"
+css_link_dom.href = "PATH_TO_COMPILED_CSS"
+document.head.append(css_link_dom)`
+					// TODO: find a way to figure out the path of the compiled css before hand
+					// return {
+					// 	loader: "js",
+					// 	contents: "",
+					// 	then: (value: esbuild.OnLoadResult) => {
+					// 		value
+					// 	}
+					// }
+				}
+				return { loader: "css", contents: css_content }
+			}
+
 			build.onLoad({ filter: /\.css$/, namespace: "file" }, async (args: esbuild.OnLoadArgs) => {
 				const contents = await Deno.readTextFile(args.path)
-				return { loader: "css", contents }
+				// TODO: apply transformation only if resolver's `args.kind === "import-statement"`.
+				// unfortunately, the `kind` member isn't passed onto the loader's `args`. so I cannot check directly
+				return transform_loaded_css(contents)
 			})
 
 			const http_loader = async (args: esbuild.OnLoadArgs): Promise<esbuild.OnLoadResult> => {
 				const contents = await (await fetch(`${args.namespace}:${args.path}`)).text()
-				return { loader: "css", contents }
+				// TODO: apply transformation only if resolver's `args.kind === "import-statement"`.
+				// unfortunately, the `kind` member isn't passed onto the loader's `args`. so I cannot check directly
+				return transform_loaded_css(contents)
 			}
 
 			build.onLoad({ filter: /\.css$/, namespace: "data" }, http_loader)
@@ -28,109 +77,3 @@ export const denoCssPlugin = (): esbuild.Plugin => {
 		}
 	}
 }
-
-export const injectCssinJs = (
-	build_results: esbuild.BuildResult<{ bundle: true, write: false }>,
-	bundle: boolean = true,
-) => {
-	const outputFiles = build_results.outputFiles
-		?.filter((output_file) => { return !output_file.path.endsWith(".css") })
-		?? []
-	const cssJsOutputFiles = build_results.outputFiles
-		?.filter((output_file) => { return output_file.path.endsWith(".css") })
-		.map((css_output_file) => {
-			const
-				{ path, text, hash } = css_output_file,
-				js_path = path.replace(/\.css$/, ".js"),
-				js_text = `
-const style_dom = document.createElement("style")
-style_dom.textContent = \`${text}\`
-document.head.append(style_dom)`,
-				js_content = new TextEncoder().encode(js_text)
-			return {
-				path: js_path,
-				text: js_text,
-				contents: js_content,
-				hash,
-			} as esbuild.OutputFile
-		}) ?? []
-	if (bundle && cssJsOutputFiles.length > 0) {
-		const
-			bundled_js_file = outputFiles.pop()!,
-			bundled_content = concatBytes(bundled_js_file.contents, ...cssJsOutputFiles!.map((css_js_injector_file) => css_js_injector_file.contents))
-		outputFiles.push({
-			path: bundled_js_file.path,
-			hash: bundled_js_file.hash,
-			contents: bundled_content,
-			text: new TextDecoder().decode(bundled_content)
-		})
-	}
-	build_results.outputFiles.splice(0)
-	build_results.outputFiles.push(...outputFiles)
-	return build_results
-}
-
-
-// export const denoCssToJsPlugin = (options: PluginOptions = {}): esbuild.Plugin => {
-// 	return {
-// 		name: "deno-css-to-js-plugin",
-// 		setup: (build) => {
-// 			if (options.name !== undefined && options.name.length > 0) {
-// 				build.onEnd(async (result: esbuild.BuildResult<esbuild.BuildOptions>): Promise<esbuild.OnEndResult> => {
-// 					console.log("name options", result.outputFiles)
-// 					return {}
-
-// 					for (const output_file of result.outputFiles ?? []) {
-// 						if (output_file.path.endsWith(".css")) {
-// 							const dirname = pathDirname(output_file.path)
-// 							output_file.path = pathJoin(dirname, options.name!)
-// 						}
-// 					}
-// 					return result
-// 				})
-// 			}
-
-// 			if (options.inject) {
-// 				build.onEnd(async (result: esbuild.BuildResult<esbuild.BuildOptions>): Promise<esbuild.OnEndResult> => {
-// 					console.log("inject options", result.outputFiles)
-// 					return {}
-
-// 					const outputFiles = result.outputFiles
-// 						?.filter((output_file) => { return !output_file.path.endsWith(".css") })
-// 						?? []
-// 					const cssJsOutputFiles = result.outputFiles
-// 						?.filter((output_file) => { return output_file.path.endsWith(".css") })
-// 						.map((css_output_file) => {
-// 							const
-// 								{ path, text, hash } = css_output_file,
-// 								js_path = path.replace(/\.css$/, ".js"),
-// 								js_text = `
-// const style_dom = document.createElement("style")
-// style_dom.textContent = \`${text}\`
-// document.head.append(style_dom)`,
-// 								js_content = new TextEncoder().encode(js_text)
-// 							return {
-// 								path: js_path,
-// 								text: js_text,
-// 								contents: js_content,
-// 								hash,
-// 							} as esbuild.OutputFile
-// 						}) ?? []
-// 					if (build.initialOptions.bundle && cssJsOutputFiles.length > 0) {
-// 						console.log(outputFiles)
-// 						const
-// 							bundled_js_file = outputFiles.pop()!,
-// 							bundled_content = concatBytes(bundled_js_file.contents, ...cssJsOutputFiles!.map((css_js_injector_file) => css_js_injector_file.contents))
-// 						outputFiles.push({
-// 							path: bundled_js_file.path,
-// 							hash: bundled_js_file.hash,
-// 							contents: bundled_content,
-// 							text: new TextDecoder().decode(bundled_content)
-// 						})
-// 					}
-// 					return result
-// 				})
-// 			}
-// 		}
-// 	}
-// }
