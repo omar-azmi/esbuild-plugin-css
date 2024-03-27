5a483460442e6b08a7671aecafe000fea24b8644
omar-azmi <omar-azmi@users.noreply.github.com>
2024-03-27 17:51:09 +0000

    Deploying to gh-pages from @ omar-azmi/esbuild-plugin-css@d45856e7354aa68e4974d05cc2514dc97119e3cd 🚀

--- src/deps.ts
+++ src/deps.ts
@@ -3,8 +3,8 @@ export type * as esbuild from "npm:esbuild@0.20.1"
 /** flags used for minifying (or eliminating) debugging logs and asserts, when an intelligent bundler, such as `esbuild`, is used. */
 export const enum DEBUG {
 	LOG = 0,
-	ASSERT = 0,
+	ASSERT = 1,
 	ERROR = 1,
 	PRODUCTION = 1,
-	MINIFY = 1,
+	MINIFY = 0,
 }
