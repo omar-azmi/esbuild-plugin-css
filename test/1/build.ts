0be2e0e441b8075abc14ec67ce22817a325c43c7
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-10-08 00:40:07 -0400

    the `esbuild` type definitions from "deno.land/x" is incompatible with the "npmjs.org" release. as such, the type-checker throws an error when running tests via `deno task test`. thus, we mutate the type of pur plugins `as any` to bypass this issue.

--- test/1/build.ts
+++ test/1/build.ts
@@ -3,15 +3,15 @@ const current_test_dir = import.meta.dirname
 if (current_test_dir) { Deno.chdir(current_test_dir) }
 else { throw new Error("could not figure out the test directory's path") }
 
-import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.9.0"
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.10.3"
 import { emptyDir } from "jsr:@std/fs"
 import { cssPlugin } from "../../src/mod.ts"
 // a dynamic import is necessary, otherwise deno will move the static import to top level during runtime, bypassing our directory change
-const esbuild = await import("https://deno.land/x/esbuild@v0.20.1/mod.js")
+const esbuild = await import("https://deno.land/x/esbuild@v0.24.0/mod.js")
 
 await emptyDir("./output/")
 const result = await esbuild.build({
-	plugins: [cssPlugin({ mode: "inject" }), ...denoPlugins()],
+	plugins: [cssPlugin({ mode: "inject" }) as any, ...denoPlugins() as any],
 	entryPoints: ["./index.ts"],
 	outdir: "./output/",
 	format: "esm",
